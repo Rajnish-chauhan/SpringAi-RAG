e1f2e9faaa6200eb91246b8cf2ae8678a496ede5
Rajnish-chauhan <crajnish425@gmail.com>
2026-09-24 00:26:40 +0530

    RAG

---
+++ src/main/java/com/rajnishsystems/in/rag/RagApplication.java
@@ -0,0 +1,13 @@
+package com.rajnishsystems.in.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagApplication.class, args);
+    }
+
+}
